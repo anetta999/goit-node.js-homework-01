65d0a1f9312a1f52a8f89c999d4ca962353493bd
anetta999 <124580023+anetta999@users.noreply.github.com>
2023-11-03 18:21:01 +0200

    complete CLI App

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # goit-node.js-homework-01
+
+## Get list of contacts
+
+![List-of-contacts-CLI](https://prnt.sc/2Vw3BnLNgJUC)
+
+## Get one contact by ID
+
+![List-of-contacts-CLI](https://prnt.sc/WXw8N36b3PNs)
+
+## Add new contact
+
+![List-of-contacts-CLI](https://prnt.sc/5vVSS6QUwSGH)
+
+## Remove contact
+
+![List-of-contacts-CLI](https://prnt.sc/UrEnFb-dEUBj)
--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Welcome to Node.js, friends!");
+import * as contactsService from "./contacts.js";
+import { program } from "commander";
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contacts = await contactsService.listContacts();
+      return console.table(contacts);
+    case "get":
+      const contact = await contactsService.getContactById(id);
+      return console.log(contact);
+    case "remove":
+      const deletedContact = await contactsService.removeContact(id);
+      return console.log(deletedContact);
+    case "add":
+      const addedContact = await contactsService.addContact(name, email, phone);
+      return console.log(addedContact);
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+program
+  .option("-a, --action <type>")
+  .option("-i, --id <type>")
+  .option("-n, --name <type>")
+  .option("-e, --email <type>")
+  .option("-p, --phone <type>");
+
+program.parse();
+
+const options = program.opts();
+invokeAction(options);
